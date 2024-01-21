bc68ec01448d21da86a4361c6b8c14a5492aca93
Leniors <mokualeeroy@gmail.com>
2024-01-21 04:17:07 +0300

    Place

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the City class."""
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -15,3 +16,4 @@ class City(BaseModel, Base):
 
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities', cascade='all, delete-orphan')
