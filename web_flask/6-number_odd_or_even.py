4f9fe21be866e040e8efd264aa2d0b5f35de8724
Leniors <mokualeeroy@gmail.com>
2024-02-17 21:28:11 +0300

    template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,6 +46,16 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ number_template/<n> route for Flask Appliction """
+    if n % 2 == 0:
+        content = f"Number: {n} is even"
+    else:
+        content = f"Number: {n} is odd"
+    return render_template('5-number.html', content=content)
+
+
 if __name__ == "__main__":
     """ only when called """
     app.run(host='0.0.0.0', port=5000)
