e1e57f58ba2d9d585346ba4b0a05de18a126855f
Leniors <mokualeeroy@gmail.com>
2024-02-17 14:12:01 +0300

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Flask Application"""
 from flask import Flask
+""" Flask Application """
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """ root route for Flask Appliction """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run()
